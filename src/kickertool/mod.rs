f50d568d12b1a7c33502ad496077048e0189a4c5
Eric Chu <ericchu@patchy.dev>
2022-05-16 01:57:12 +0800

    Refactor table logic

--- src/kickertool/mod.rs
+++ src/kickertool/mod.rs
@@ -1,6 +1,6 @@
 mod scraper;
 
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use rxrust::prelude::*;
 
@@ -15,7 +15,7 @@ type KickertoolDataObservable =
     impl Clone + Observable<Item = KickertoolData, Err = ()> + SharedObservable;
 
 pub struct Kickertool {
-    team_subscriptions: [Option<Box<dyn SubscriptionLike>>; 2],
+    team_subscriptions: HashMap<(u8, usize), Box<dyn SubscriptionLike>>,
     standings_subscription: Option<Box<dyn SubscriptionLike>>,
     kickertool_data_observable: KickertoolDataObservable,
 }
@@ -49,19 +49,19 @@ impl Kickertool {
     pub fn new(url_html_observable: UrlHtmlObservable) -> Self {
         let kickertool_data_observable = get_kickertool_data_observable(url_html_observable);
         let mut s = Self {
-            team_subscriptions: [None, None],
+            team_subscriptions: HashMap::new(),
             standings_subscription: None,
             kickertool_data_observable,
         };
         s.standings_subscribe();
-        s.team_subscribe(1);
-        s.team_subscribe(2);
+        s.team_subscribe(1, 1);
+        s.team_subscribe(1, 2);
 
         s
     }
 
     fn standings_subscribe(&mut self) {
-        Self::unsubscribe(&mut self.standings_subscription);
+        Self::unsubscribe(self.standings_subscription.as_mut());
 
         let sink = FileSink::new("standings.txt");
 
@@ -79,57 +79,59 @@ impl Kickertool {
         self.standings_subscription = (Box::new(s) as Box<dyn SubscriptionLike>).into();
     }
 
-    fn get_table1_observable(&self) -> Table1Observable {
+    fn get_table_observable(&self, number: u8) -> Table1Observable {
         self.kickertool_data_observable
             .clone()
-            .filter_map(|data: KickertoolData| {
-                data.tables.into_iter().find(|table| table.number == 1)
+            .filter_map(move |data: KickertoolData| {
+                data.tables.into_iter().find(|table| table.number == number)
             })
     }
 
-    fn team_subscribe(&mut self, number: usize) {
-        self.team_unsubscribe(number);
+    fn team_subscribe(&mut self, table_number: u8, team_number: usize) {
+        self.team_unsubscribe(table_number, team_number);
 
-        let sink = FileSink::new(format!("team{number}.txt"));
+        let sink = FileSink::new(format!("table{table_number}-team{team_number}.txt"));
 
         let s = self
-            .get_table1_observable()
-            .map(move |data| match number {
+            .get_table_observable(1)
+            .map(move |data| match team_number {
                 1 => data.team1,
                 2 => data.team2,
                 _ => unreachable!(),
             })
             .distinct_until_changed()
-            .tap(move |team| println!("Team{number}: {team}"))
+            .tap(move |team| println!("Table{table_number} Team{team_number}: {team}"))
             .into_shared()
             .subscribe(move |standings| sink.sink(standings));
 
-        let subscription = self.get_team_subscription_mut(number);
-        *subscription = (Box::new(s) as Box<dyn SubscriptionLike>).into();
+        let mut subscription = self.get_team_subscription_mut(table_number, team_number);
+        subscription.replace(&mut (Box::new(s) as Box<dyn SubscriptionLike>));
     }
 
-    fn team_unsubscribe(&mut self, number: usize) {
-        Self::unsubscribe(self.get_team_subscription_mut(number));
+    fn team_unsubscribe(&mut self, table_number: u8, team_number: usize) {
+        Self::unsubscribe(self.get_team_subscription_mut(table_number, team_number));
     }
 
-    fn unsubscribe(subscription: &mut Option<Box<dyn SubscriptionLike>>) {
-        if let Some(mut subscription) = subscription.take() {
+    fn unsubscribe(mut subscription: Option<&mut Box<dyn SubscriptionLike>>) {
+        if let Some(subscription) = subscription.take() {
             subscription.unsubscribe();
         }
     }
 
     fn get_team_subscription_mut(
         &mut self,
-        number: usize,
-    ) -> &mut Option<Box<dyn SubscriptionLike>> {
-        &mut self.team_subscriptions[number - 1]
+        table_number: u8,
+        team_number: usize,
+    ) -> Option<&mut Box<dyn SubscriptionLike>> {
+        self.team_subscriptions
+            .get_mut(&(table_number, team_number))
     }
 }
 
 impl Drop for Kickertool {
     fn drop(&mut self) {
-        self.team_unsubscribe(1);
-        self.team_unsubscribe(2);
-        Self::unsubscribe(&mut self.standings_subscription);
+        self.team_unsubscribe(1, 1);
+        self.team_unsubscribe(1, 2);
+        Self::unsubscribe(self.standings_subscription.as_mut());
     }
 }
