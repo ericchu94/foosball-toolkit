2695b204f71e85bd7b0dcf63bd302477645a93b4
Eric Chu <ericchu@patchy.dev>
2022-05-21 03:56:51 +0800

    Make base_url public

--- foos-db-app/src/hooks/mod.rs
+++ foos-db-app/src/hooks/mod.rs
@@ -8,4 +8,4 @@ pub use use_player_matches::*;
 pub use use_player::*;
 pub use use_matches::use_matches;
 
-const BASE_URL: &str = "https://foos-db.herokuapp.com";
+pub const BASE_URL: &str = "https://foos-db.herokuapp.com";
