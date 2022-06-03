947003ff567b1f4ed29c0043fd8534b72758d642
Eric Chu <eric@ericchu.net>
2022-06-03 11:06:39 +0800

    Remove unused queries

--- foos-db/src/database/mod.rs
+++ foos-db/src/database/mod.rs
@@ -117,24 +117,6 @@ impl Database {
             .collect())
     }
 
-    pub async fn get_or_create_player(&self, player: Player) -> Result<Player> {
-        query!(
-            "INSERT INTO player (first_name, last_name) VALUES ($1, $2) ON CONFLICT DO NOTHING",
-            player.first_name,
-            player.last_name
-        )
-        .execute(&self.pool)
-        .await?;
-        Ok(query_as!(
-            Player,
-            "SELECT * FROM player WHERE first_name = $1 AND last_name = $2",
-            player.first_name,
-            player.last_name
-        )
-        .fetch_one(&self.pool)
-        .await?)
-    }
-
     pub async fn merge_and_delete_player(&self, from: i32, to: i32) -> Result<()> {
         self.merge_player(from, to).await?;
 
@@ -209,18 +191,6 @@ impl Database {
         .await?)
     }
 
-    pub async fn create_match(&self, r#match: Match) -> Result<Match> {
-        Ok(query_as_unchecked!(
-            Match,
-            "INSERT INTO match (tournament_id, timestamp, winner) VALUES ($1, $2, $3) RETURNING *",
-            r#match.tournament_id,
-            r#match.timestamp,
-            r#match.winner
-        )
-        .fetch_one(&self.pool)
-        .await?)
-    }
-
     pub async fn create_matches(&self, matches: &[Match]) -> Result<Vec<Match>> {
         let tournament_ids = matches
             .iter()
@@ -243,37 +213,6 @@ impl Database {
         )
         .fetch_all(&self.pool)
         .await?)
-    }
-
-    pub async fn create_match_and_players(
-        &self,
-        r#match: Match,
-        team1: Vec<Player>,
-        team2: Vec<Player>,
-    ) -> Result<Match> {
-        let r#match = self.create_match(r#match).await?;
-
-        for player in team1 {
-            let player = self.get_or_create_player(player).await?;
-            let player_match = PlayerMatch {
-                player_id: player.id,
-                match_id: r#match.id,
-                team: Team::Team1,
-            };
-            self.create_player_match(player_match).await?;
-        }
-
-        for player in team2 {
-            let player = self.get_or_create_player(player).await?;
-            let player_match = PlayerMatch {
-                player_id: player.id,
-                match_id: r#match.id,
-                team: Team::Team2,
-            };
-            self.create_player_match(player_match).await?;
-        }
-
-        Ok(r#match)
     }
 
     pub async fn create_games(&self, games: Vec<Game>) -> Result<()> {
@@ -303,19 +242,6 @@ impl Database {
         .await?;
 
         Ok(games)
-    }
-
-    pub async fn create_player_match(&self, player_match: PlayerMatch) -> Result<()> {
-        query!(
-            "INSERT INTO player_match (player_id, match_id, team) VALUES ($1, $2, $3)",
-            player_match.player_id,
-            player_match.match_id,
-            player_match.team as Team
-        )
-        .execute(&self.pool)
-        .await?;
-
-        Ok(())
     }
 
     pub async fn create_player_matches(&self, player_matches: &[PlayerMatch]) -> Result<()> {
